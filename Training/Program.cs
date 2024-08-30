3c7bc75e230c130407ce9efaf2714088b88f3b4b
Cristian Mancuso <cristian.mancuso1@outlook.com>
2024-08-30 17:06:14 +0200

    Added ASP Blazor training

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,8 +1,17 @@
-﻿public class Training
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Windows.Forms;
+
+public class Training
 {
     static void Main()
     {
         Console.WriteLine("Test second commit");
+        Car car = new Car();
+        car.color = "new_red";
+        Console.WriteLine(car.color);
+
+        Form myform = new Form();
     }
 }
 
